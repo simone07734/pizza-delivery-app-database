b0dd1f6dfd7d57ac08bf6e5f772f25f31d4a073b
simone07734 <simone.m.laney@gmail.com>
2024-11-17 16:36:41 -0800

    added comments

--- phase2.sql
+++ phase2.sql
@@ -39,10 +39,10 @@ CREATE TABLE views (
     login CHAR(15) NOT NULL,
     itemName CHAR(50) NOT NULL,
     PRIMARY KEY (login, itemName),
-    FOREIGN KEY (login) REFERENCES AppUser
+    FOREIGN KEY (login) REFERENCES AppUser --if the user is gone then so is their view
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (itemName) REFERENCES Item 
+    FOREIGN KEY (itemName) REFERENCES Item  --if an item is gone then the user can't view it
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -62,32 +62,34 @@ CREATE TABLE OrderPlaced (
     login CHAR(15) NOT NULL,
     storeID CHAR(50) NOT NULL,
     itemName CHAR(50) NOT NULL,
-    --participation constraint
-    CONSTRAINT hasForeignKey 
-        FOREIGN KEY (orderID,itemName)
-        REFERENCES has(orderID, itemName) DEFERRABLE INITIALLY DEFERRED,
-
     PRIMARY KEY (orderID),
-    FOREIGN KEY (login) REFERENCES AppUser
+    FOREIGN KEY (login) REFERENCES AppUser --if a user is gone then so are their orders
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (storeID) REFERENCES Store
+    FOREIGN KEY (storeID) REFERENCES Store -- if a store is gone then so are its orders
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    
+    --OrderPlaced has full participation in has
+    CONSTRAINT hasForeignKey 
+        FOREIGN KEY (orderID,itemName)
+        REFERENCES has(orderID, itemName) DEFERRABLE INITIALLY DEFERRED
 );
 
+--add reference after OrderPlaced is created
 ALTER TABLE has
     ADD FOREIGN KEY (orderID) REFERENCES OrderPlaced
         ON DELETE CASCADE
         ON UPDATE CASCADE;
 
-
 CREATE TABLE availableAt (
     itemName CHAR(50) NOT NULL,
     storeID CHAR(50) NOT NULL,
     PRIMARY KEY (itemName, storeID),
-    FOREIGN KEY (itemName) REFERENCES Item
+    FOREIGN KEY (itemName) REFERENCES Item --should reflect the current Items
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (storeID) REFERENCES Store
+    FOREIGN KEY (storeID) REFERENCES Store --should reflect the current Stores
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
